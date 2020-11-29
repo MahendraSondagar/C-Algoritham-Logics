9948e7c9fd05924f096ea5668e8e0b37a9af7cef
MahendraSondagar <mahendrasondagar08@gmail.com>
2020-11-30 00:00:45 +0530

    PRINTF macro - preprocessor

---
+++ preprocessor/PRINTF_macro.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define BIO     "Hello world\r\n"                             \
+                "I'm Mahendra Sondagar\r\n"                   \
+                "I'm an IoT geek\r\n"                         \
+                "Connect me on <mahendrasondagar08@gmail.com>" 
+
+int main(void)
+{
+    printf(BIO);
+}
